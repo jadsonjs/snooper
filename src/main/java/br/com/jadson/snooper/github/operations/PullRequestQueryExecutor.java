c53275dffc5dcdce11d3e1434fa60caedf61d777
BrunoKaike <78989493+BrunoKaike@users.noreply.github.com>
2021-07-20 11:00:31 -0300

    imports

--- src/main/java/br/com/jadson/snooper/github/operations/PullRequestQueryExecutor.java
+++ src/main/java/br/com/jadson/snooper/github/operations/PullRequestQueryExecutor.java
@@ -38,6 +38,8 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Iterator;
+import java.time.ZoneId;
 
 /**
  * Queries about Pull Request
