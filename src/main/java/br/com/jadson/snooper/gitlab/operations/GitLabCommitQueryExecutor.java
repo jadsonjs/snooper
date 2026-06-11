7a0b91b11e4ca9b20694b733f09a0016eb9f5c68
Luisa <luisaferreirass08@gmail.com>
2026-06-11 09:41:40 -0300

    feat: implementing getCommitFiles to GitlLabCommitQueryExecutor

--- src/main/java/br/com/jadson/snooper/gitlab/operations/GitLabCommitQueryExecutor.java
+++ src/main/java/br/com/jadson/snooper/gitlab/operations/GitLabCommitQueryExecutor.java
@@ -1,7 +1,8 @@
 package br.com.jadson.snooper.gitlab.operations;
 
-import br.com.jadson.snooper.github.data.commit.GitHubCommitInfo;
+import br.com.jadson.snooper.gitlab.data.commit.CommitDiff;
 import br.com.jadson.snooper.gitlab.data.commit.GitLabCommitInfo;
+import br.com.jadson.snooper.gitlab.data.commit.FileChanged;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -80,4 +81,85 @@ public class GitLabCommitQueryExecutor extends AbstractGitLabQueryExecutor {
         return allPull;
     }
 
-}
+    public List<FileChanged> getCommitsFiles(String repoFullName, GitLabCommitInfo commit){
+        validateRepoName(repoFullName);
+
+        ResponseEntity<CommitDiff[]> result;
+
+        String query = getGitLabAPIURL()
+                + encodeProjectName(repoFullName)
+                + "/repository/commits/"
+                + commit.id
+                + "/diff";
+
+        System.out.println(query);
+
+        System.out.println("Getting files for GitLab commit");
+
+        URI uri = null;
+        try {
+            uri = new URI(query);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        checkDisableSslVerification();
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity entity = new HttpEntity(this.getDefaultHeaders());
+        result = restTemplate.exchange(uri, HttpMethod.GET, entity, CommitDiff[].class);
+
+
+
+        if (result.getBody() == null) {
+            return new ArrayList<>();
+        }
+
+        List<FileChanged> files = new ArrayList<>();
+
+        for (CommitDiff diff : result.getBody()) {
+            FileChanged file = new FileChanged();
+
+            file.filename = diff.newPath != null ? diff.newPath : diff.oldPath;
+            file.additions = countAdditions(diff.diff);
+            file.deletions = countDeletions(diff.diff);
+
+            files.add(file);
+        }
+
+        return files;
+    }
+
+    private int countAdditions(String diff){
+        if (diff == null || diff.isEmpty()) {
+            return 0;
+        }
+
+        int additions = 0;
+
+        for (String line : diff.split("\n")) {
+            if (line.startsWith("+") && !line.startsWith("+++")) {
+                additions++;
+            }
+        }
+
+        return additions;
+    }
+
+    private int countDeletions(String diff) {
+        if (diff == null || diff.isEmpty()) {
+            return 0;
+        }
+
+        int deletions = 0;
+
+        for (String line : diff.split("\n")) {
+            if (line.startsWith("-") && !line.startsWith("---")) {
+                deletions++;
+            }
+        }
+
+        return deletions;
+    }
+    }
